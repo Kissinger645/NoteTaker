1440e91816769f971d9a42bc0852b1f866e1740c
Bobby <kissinger645@gmail.com>
2017-02-02 09:53:47 -0500

    Started EF models and context

---
+++ NoteTaker/Migrations/201702021450100_Initial.cs
@@ -0,0 +1,29 @@
+namespace NoteTaker.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Notes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Body = c.String(),
+                        TimeStamp = c.String(),
+                        Created = c.DateTime(nullable: false),
+                        Title = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Notes");
+        }
+    }
+}
---
+++ NoteTaker/NoteContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using NoteTaker.Models;
+
+namespace NoteTaker
+{
+    public class NoteContext : DbContext
+
+    {
+        public DbSet<Note> Notes { get; set; }
+    }
+}
